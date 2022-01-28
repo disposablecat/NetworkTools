e2b4890c2dc6da5c7c9c044e0cb1fa3271a4021d
disposablecat <78448375+disposablecat@users.noreply.github.com>
2022-01-27 21:09:31 -0500

    Initial commit

---
+++ NetworkTools.psm1
@@ -0,0 +1,102 @@
+﻿function Test-Ports{
+<#
+.SYNOPSIS
+    Test whether a TCP port or ports are open on a computer or computers
+.DESCRIPTION
+    Test whether a TCP port or ports are open on a computer or computers. Returns whether the port or ports are opened or closed. 
+.PARAMETER ComputerName
+    Specify a computer to connect to.
+.PARAMETER Ports
+    Specify a TCP port or ports to test
+.NOTES
+    Version:        1.0
+    Author:         disposablecat
+    Purpose/Change: Initial script development
+.EXAMPLE
+    Test-Ports -ComputerName server1 -ports 80
+    Will return if the TCP port is open on server1, assuming it can resolve the DNS name to an IP.
+.EXAMPLE
+    Test-Ports -ComputerName server1 -ports 80,443,3389
+    Will return if the TCP ports are open on server1, assuming it can resolve the DNS name to an IP.
+.EXAMPLE
+    Test-Ports -ComputerName server1,server2,10.11.107.52 -ports 80,443,3389
+    Will return if the TCP ports are open on server1, server2, and 10.11.107.52, assuming it can resolve the DNS name to an IP for the first two.
+    It will try the IP specify regardless.
+#>
+    [CmdletBinding()]
+    [OutputType([System.Object])]
+    
+    #Define parameters
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$ComputerName,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [ValidateRange(1,65535)]
+        [int[]]$Ports,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [ValidateRange(1,10000)]
+        [int]$Timeout = 100
+    )
+
+    Begin
+    {
+        $PortsObjectBase = New-Object PSObject; 
+        $PortsObjectBase | Add-Member -type Noteproperty -Name ComputerName -Value $Null
+        $PortsObjectBase | Add-Member -type Noteproperty -Name IP -Value $Null
+        $PortsObjectBase | Add-Member -type Noteproperty -Name Port -Value $Null
+        $PortsObjectBase | Add-Member -type Noteproperty -Name Open -Value $Null
+        $Results = New-Object System.Collections.Generic.List[System.Object]    
+    }
+    Process
+    {
+        ForEach ($Computer in $ComputerName)
+        {
+            
+            try
+            {
+                $Resolved = [System.Net.Dns]::GetHostEntry($Computer)
+                $IPAddress = $Resolved.AddressList.IPAddressToString
+                $Name = $Resolved.HostName
+            }
+            Catch
+            {
+                #Catch any error.
+                Write-Verbose “Exception Caught: $($_.Exception.Message)”
+            }
+
+            ForEach ($P in $Ports)
+            {
+                $PortsObject = $PortsObjectBase | Select *
+                $PortsObject.ComputerName = $Name
+                $PortsObject.IP = $IPAddress
+                $PortsObject.Port = $P
+                $Socket = New-Object Net.Sockets.TcpClient
+                $Connect = $Socket.BeginConnect($IPAddress,$P,$Null,$Null)
+                $Wait = $Connect.AsyncWaitHandle.WaitOne($Timeout,$false)
+                if($Wait -eq $false)
+                {
+                    $Socket.Close()
+                    $PortsObject.Open = $false
+                    Write-Verbose "Port $P on $Computer did not respond try increasing timeout."
+                }
+                else
+                {
+                    $Socket.EndConnect($Connect) | Out-Null
+                    $PortsObject.Open = $true
+                }
+                $Results.Add($PortsObject)
+            }            
+        }
+        return $Results
+    }
+    End
+    {
+        #Will execute last. Will execute once. Good for cleanup. 
+    }
+}
+
---
+++ Test-ConnectionFast.psm1
@@ -0,0 +1,99 @@
+﻿#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+function Test-ConnectionFast{
+<#
+.SYNOPSIS
+    Pings a host or hosts
+.DESCRIPTION
+    Quickly pings a hosts or host. Test if a host up much more quickly than Test-Connection, especially if the host is unreachable. 
+.PARAMETER ComputerName
+    Parameter description
+.PARAMETER Timeout
+    Parameter description
+.NOTES
+    Version:        1.0
+    Author:         disposablecat
+    Purpose/Change: Initial script development
+.EXAMPLE
+    Example of how to use this cmdlet
+.EXAMPLE
+    Another example of how to use this cmdlet
+#>
+    [CmdletBinding()]
+    [OutputType([int])]
+    
+    #Define parameters
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$ComputerName,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [int]$Timeout = 50
+    )
+
+    Begin
+    {
+        #Will execute first. Will execute one time only. Use for one time actions
+        $PingResponseBase = New-Object PSObject; 
+        $PingResponseBase | Add-Member -type Noteproperty -name ComputerName -Value $Null
+        $PingResponseBase | Add-Member -type Noteproperty -name IP -Value $Null;
+        $PingResponseBase | Add-Member -type Noteproperty -name RTT -Value $Null
+        $PingResponseBase | Add-Member -type Noteproperty -name Result -Value $Null;
+        $Pings = New-Object System.Collections.ArrayList
+        $Results = New-Object System.Collections.Generic.List[System.Object]
+    }
+    Process
+    {
+        Write-Verbose "Starting pings tasks."
+        ForEach($Computer in $ComputerName)
+        {
+            Try
+            {
+                Write-Verbose "Starting ping of $Computer."
+                [void]$Pings.Add((New-Object Net.NetworkInformation.Ping).SendPingAsync($Computer, $Timeout))
+                $PingResponse = $PingResponseBase | Select *
+                $PingResponse.ComputerName = $Computer
+                [void]$Results.Add($PingResponse)
+
+            }
+            Catch
+            {
+                #Catch any error.
+                Write-Verbose “Exception Caught: $($_.Exception.Message)”
+            }
+        }
+    }
+    End
+    {
+        Write-Verbose "Waiting for ping tasks to complete."
+        Try
+        {
+            [void][Threading.Tasks.Task]::WaitAll($Pings)
+        }
+        Catch [System.Management.Automation.MethodInvocationException]
+        {
+            #Catch any error.
+            Write-Verbose “Exception Caught: Could not lookup one or more hosts. See results for No such host is known message.”
+        }
+        $i = 0
+        ForEach($Ping in $Pings)
+        {
+            if($Ping.IsFaulted)
+            {
+                $Results[$i].IP = $null
+                $Results[$i].RTT = $null
+                $Results[$i].Result = [string]$Ping.Exception.InnerException.InnerException.Message
+            }
+            else
+            {
+                $Results[$i].IP = $Ping.Result.Address.ToString()
+                $Results[$i].RTT = $Ping.Result.RoundtripTime
+                $Results[$i].Result = $Ping.Result.Status 
+            }
+            $i++
+        }
+        return $Results
+
+    }
+}
